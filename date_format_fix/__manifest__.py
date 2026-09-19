b0b48a280b082a7a142b00e244032f462c870d63
MrFenix93 <mohamedallam93@gmail.com>
2026-09-19 20:19:36 +0300

    Add banner

--- date_format_fix/__manifest__.py
+++ date_format_fix/__manifest__.py
@@ -3,14 +3,21 @@
     "version": "19.0.1.0.0",
     "summary": "Force dd/MM/yyyy date format display in Odoo 19",
     "author": "Ato Solution",
-    "depends": ["web"],
-    "license": "LGPL-3",
     "website": "https://ato-solution.com",
+    "license": "LGPL-3",
+
+    "depends": ["web"],
+
+    "images": [
+        "static/description/banner.png",
+    ],
+
     "assets": {
         "web.assets_backend": [
             "date_format_fix/static/src/js/date_format_patch.js",
         ],
     },
+
     "installable": True,
     "application": False,
     "post_init_hook": "post_init_hook",
